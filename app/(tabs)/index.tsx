0f5d3dfae6a29b72b9d357212f65d3e6c1a249ed
Adekoya Daniel <adekoyadaniel53@gmail.com>
2024-09-25 21:55:09 +0100

    blood of jesus navigation scroll

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,139 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  View,
+  Animated,
+} from "react-native";
+import * as Haptics from "expo-haptics";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { useRef, useState } from "react";
 
 export default function HomeScreen() {
+  const data = [
+    "Clubs",
+    "Lounges",
+    "Cafes",
+    "Clubbs",
+    "Loungges",
+    "Caffes",
+    "Clurbs",
+    "Loungees",
+    "Cafeas",
+    "Cafeass",
+  ];
+
+  Animated.const[(active, setActive)] = useState(0);
+  const flatListRef = useRef<FlatList<string>>(null);
+
+  /*   const onChange = useRef(({ viewableItems }: any) => {
+    if (viewableItems.length > 0) {
+      console.log("Visible items:", viewableItems);
+      const firstViewableItem = viewableItems[0];
+      setActive(firstViewableItem.index);
+      Haptics.selectionAsync();
+    }
+  }).current; */
+  /*   const scrollToItem = (index: number) => {
+    if (flatListRef.current) {
+      flatListRef.current.scrollToIndex({ index, animated: false });
+    }
+  }; */
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      }
+    >
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item}
+        horizontal
+        ref={flatListRef}
+        //onViewableItemsChanged={onChange}
+        showsHorizontalScrollIndicator={false}
+        viewabilityConfig={{
+          itemVisiblePercentThreshold: 50,
+          minimumViewTime: 200,
+        }}
+        style={styles.container}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            onPress={() => {
+              setActive(index);
+              Haptics.selectionAsync();
+              // scrollToItem(index);
+            }}
+          >
+            <View style={active === index ? styles.pinActive : styles.pin}>
+              <Text
+                style={active === index ? styles.pinTextActive : styles.pinText}
+              >
+                {item}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        onScroll={({ nativeEvent }) => {
+          const offsetX = nativeEvent.contentOffset.x;
+          const viewableItems = data.map((_, i) => {
+            return {
+              index: i,
+              isVisible: offsetX >= i * 44 && offsetX < (i + 1) * 44,
+            };
+          });
+          const visibleItem = viewableItems.find((item) => item.isVisible);
+          if (visibleItem) {
+            setActive(visibleItem.index);
+          }
+        }}
+      />
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  container: {
+    paddingVertical: 10,
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+  pin: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderColor: "#6c6c6c",
+    borderRadius: 999,
+    alignSelf: "flex-start",
+    marginLeft: 7,
+  },
+  pinText: {
+    color: "#6c6c6c",
+  },
+  pinActive: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderColor: "#fff",
+    backgroundColor: "#fff",
+    borderRadius: 999,
+    alignSelf: "flex-start",
+    transform: [{ rotate: "-10deg" }],
+    marginLeft: 7,
+  },
+  pinTextActive: {
+    color: "#000",
   },
 });
